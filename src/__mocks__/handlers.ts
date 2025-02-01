ebb65f30724c466e5736e2f18bf3c15652b89fa7
keyonnaise <su.smisc14@gmail.com>
2025-02-01 23:04:45 +0900

    ✨ Mockup API 핸들러 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -6,11 +6,30 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const newEvent = (await request.json()) as Event;
+    newEvent.id = `${events.length + 1}`;
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
 
-  http.put('/api/events/:id', async () => {}),
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const { id } = params;
+    const modifiedEvent = (await request.json()) as Event;
+    const index = events.findIndex((event) => event.id === id);
+    return index !== -1
+      ? HttpResponse.json(modifiedEvent, { status: 201 })
+      : HttpResponse.json(null, { status: 404 });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+    const index = events.findIndex((event) => event.id === id);
+    return index !== -1
+      ? HttpResponse.json(null, { status: 204 })
+      : HttpResponse.json(null, { status: 404 });
+  }),
 ];
--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -1,10 +1,67 @@
+import { http, HttpResponse } from 'msw';
+
+import { server } from '../setupTests';
 import { Event } from '../types';
+
+const EVENTS_ENDPOINT = '/api/events';
 
 // ! Hard
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
-export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {};
+export const setupMockHandlers = (initEvents: Event[] = []) => {
+  let mockEvents = [...initEvents];
 
-export const setupMockHandlerUpdating = () => {};
+  server.use(
+    /**
+     * GET /api/events
+     */
+    http.get(EVENTS_ENDPOINT, () => {
+      return HttpResponse.json({ events: mockEvents });
+    }),
 
-export const setupMockHandlerDeletion = () => {};
+    /**
+     * POST /api/events
+     */
+    http.post(EVENTS_ENDPOINT, async ({ request }) => {
+      const newEvent = (await request.json()) as Event;
+      newEvent.id = `${mockEvents.length + 1}`;
+
+      mockEvents = [...mockEvents, newEvent];
+
+      return HttpResponse.json(newEvent, { status: 201 });
+    }),
+
+    /**
+     * PUT /api/events/:id
+     */
+    http.put(`${EVENTS_ENDPOINT}/:id`, async ({ params, request }) => {
+      const { id } = params;
+      const modifiedEvent = (await request.json()) as Event;
+      const index = mockEvents.findIndex((event) => event.id === id);
+      return index !== -1
+        ? HttpResponse.json(modifiedEvent, { status: 201 })
+        : HttpResponse.json(null, { status: 204 });
+    }),
+
+    /**
+     * DELETE /api/events/:id
+     */
+    http.put(`${EVENTS_ENDPOINT}/:id`, ({ params }) => {
+      const { id } = params;
+      mockEvents = mockEvents.filter((event) => event.id !== id);
+      return new HttpResponse(null, { status: 204 });
+    })
+  );
+};
+
+export const setupMockHandlerCreation = (initEvents: Event[] = []) => {
+  setupMockHandlers(initEvents);
+};
+
+export const setupMockHandlerUpdating = (initEvents: Event[] = []) => {
+  setupMockHandlers(initEvents);
+};
+
+export const setupMockHandlerDeletion = (initEvents: Event[] = []) => {
+  setupMockHandlers(initEvents);
+};
