be076f9322cdbb2d845393c86adaa86a937a2504
keyonnaise <su.smisc14@gmail.com>
2025-02-04 23:30:32 +0900

    🚧 통합 테스트 작업 중

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -5,10 +5,129 @@ import { Event } from '../../types.ts';
 import { formatDate } from '../../utils/dateUtils.ts';
 import { parseHM } from '../utils.ts';
 
-it.skip('초기 상태에서는 알림이 없어야 한다', () => {});
+beforeEach(() => {
+  vi.useFakeTimers();
+});
 
-it.skip('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+afterEach(() => {
+  vi.useRealTimers();
+});
 
-it.skip('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+it('초기 상태에서는 알림이 없어야 한다', () => {
+  const { result } = renderHook(() => useNotifications([]));
 
-it.skip('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  expect(result.current.notifications).toHaveLength(0);
+});
+
+it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {
+  const now = new Date();
+  const startTime = parseHM(now.getTime() + 10 * 60 * 1000);
+
+  const event: Event = {
+    id: '1',
+    title: '다람쥐 헌 쳇바퀴에 타고파',
+    date: formatDate(now),
+    startTime: startTime,
+    endTime: '23:58',
+    description: '가느다란 몸 부수어 쥔 총칼, 터, 평화',
+    location: '장소',
+    category: '분류',
+    repeat: {
+      type: 'none',
+      interval: 0,
+    },
+    notificationTime: 10,
+  };
+
+  const { result } = renderHook(() => useNotifications([event]));
+
+  act(() => vi.advanceTimersByTime(1000));
+
+  expect(result.current.notifications).toHaveLength(1);
+});
+
+it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {
+  const now = new Date();
+  const startTime = parseHM(now.getTime() + 10 * 60 * 1000);
+
+  const event1: Event = {
+    id: '1',
+    title: '다람쥐 헌 쳇바퀴에 타고파',
+    date: formatDate(now),
+    startTime: startTime,
+    endTime: '23:58',
+    description: '가느다란 몸 부수어 쥔 총칼, 터, 평화',
+    location: '장소',
+    category: '분류',
+    repeat: {
+      type: 'none',
+      interval: 0,
+    },
+    notificationTime: 10,
+  };
+
+  const event2: Event = {
+    id: '2',
+    title: '다람쥐 헌 쳇바퀴에 타고파',
+    date: formatDate(now),
+    startTime: startTime,
+    endTime: '23:58',
+    description: '가느다란 몸 부수어 쥔 총칼, 터, 평화',
+    location: '장소',
+    category: '분류',
+    repeat: {
+      type: 'none',
+      interval: 0,
+    },
+    notificationTime: 10,
+  };
+
+  const { result } = renderHook(() => useNotifications([event1, event2]));
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(2);
+
+  act(() => {
+    result.current.removeNotification(0);
+  });
+
+  expect(result.current.notifications[0].id).toBe('2');
+});
+
+it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {
+  const now = new Date();
+  const startTime = parseHM(now.getTime() + 10 * 60 * 1000);
+
+  const event: Event = {
+    id: '1',
+    title: '다람쥐 헌 쳇바퀴에 타고파',
+    date: formatDate(now),
+    startTime: startTime,
+    endTime: '23:58',
+    description: '가느다란 몸 부수어 쥔 총칼, 터, 평화',
+    location: '장소',
+    category: '분류',
+    repeat: {
+      type: 'none',
+      interval: 0,
+    },
+    notificationTime: 10,
+  };
+
+  const { result } = renderHook(() => useNotifications([event]));
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+});
--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,21 +1,86 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { render, screen, within, act } from '@testing-library/react';
-import { UserEvent, userEvent } from '@testing-library/user-event';
+import { render, screen, within, act, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 import { ReactElement } from 'react';
 
 import App from '../App';
 import { server } from '../setupTests';
 import { Event } from '../types';
+import { setupMockHandlers } from '../__mocks__/handlersUtils';
+import { formatDate } from '../utils/dateUtils';
+import { parseHM } from './utils';
 
-describe.skip('일정 CRUD 및 기본 기능', () => {
+import { UserEvent, userEvent } from '@testing-library/user-event';
+
+let user: UserEvent;
+
+beforeEach(() => {
+  user = userEvent.setup();
+});
+
+describe('일정 CRUD 및 기본 기능', () => {
   it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
     // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+
+    setupMockHandlers();
+    render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
+
+    const now = new Date();
+    const startTime = parseHM(now.getTime() + 10 * 60 * 1000);
+
+    const form = {
+      id: '1',
+      title: '다람쥐 헌 쳇바퀴에 타고파',
+      date: formatDate(now),
+      startTime: startTime,
+      endTime: '23:58',
+      description: '가느다란 몸 부수어 쥔 총칼, 터, 평화',
+      location: '장소',
+      category: '기타',
+    };
+
+    await user.type(screen.getByLabelText('제목'), form.title);
+    await user.type(screen.getByLabelText('날짜'), form.date);
+    await user.type(screen.getByLabelText('시작 시간'), form.startTime);
+    await user.type(screen.getByLabelText('종료 시간'), form.endTime);
+    await user.type(screen.getByLabelText('설명'), form.description);
+    await user.type(screen.getByLabelText('위치'), form.location);
+    await user.type(screen.getByLabelText('카테고리'), form.category);
+
+    // Promise.all 왜 안됨?
+    // await Promise.all([
+    //   user.type(screen.getByLabelText('제목'), form.title),
+    //   user.type(screen.getByLabelText('날짜'), form.date),
+    //   user.type(screen.getByLabelText('시작 시간'), form.startTime),
+    //   user.type(screen.getByLabelText('종료 시간'), form.endTime),
+    //   user.type(screen.getByLabelText('설명'), form.description),
+    //   user.type(screen.getByLabelText('위치'), form.location),
+    //   user.type(screen.getByLabelText('카테고리'), form.category),
+    // ]);
+
+    await user.click(screen.getByRole('button', { name: '일정 추가' }));
+
+    const eventList = screen.getByTestId('event-list');
+
+    await waitFor(async () => {
+      expect(within(eventList).getByText(form.title)).toBeInTheDocument();
+      expect(within(eventList).getByText(form.date)).toBeInTheDocument();
+      expect(
+        within(eventList).getByText(`${form.startTime} - ${form.endTime}`)
+      ).toBeInTheDocument();
+      expect(within(eventList).getByText(form.description)).toBeInTheDocument();
+      expect(within(eventList).getByText(form.location)).toBeInTheDocument();
+      // expect(within(eventList).getByText(form.category)).toBeInTheDocument();
+    });
   });
 
-  it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {});
+  it.skip('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {});
 
-  it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {});
+  it.skip('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {});
 });
 
 describe.skip('일정 뷰', () => {
